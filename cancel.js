3b5107e8a9fb89e1685602c5b7dce3bc2f4aa5c8
Jan-Ivar Bruaroey <wingboy@gmail.com>
2017-01-03 20:53:29 -0500

    Polyfill fetch taking promise as {cancel} argument.

--- cancel.js
+++ cancel.js
@@ -6,3 +6,11 @@ Promise.cancel = p => {
   let pair = x => [x, x];
   return Promise.resolve(p).then(...pair(() => Promise.reject(new CancelError())));
 }
+
+// Polyfill a fetch taking a promise as a {cancel} argument.
+
+window._nativeFetch = window.fetch;
+
+window.fetch = (url, init) =>
+  Promise.race([window._nativeFetch(url, init),
+                Promise.cancel(init.cancel || new Promise())]);
